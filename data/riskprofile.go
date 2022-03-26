72a083b3ff91a2279cc0d29470a5049c334c6795
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-03-26 23:30:46 +0700

    Update riskprofile.go

--- data/riskprofile.go
+++ data/riskprofile.go
@@ -1,7 +1,7 @@
 package data
 
 type RiskProfile struct {
-	UserId 			uint 	`json:"-"`
+	UserId 			uint 	`json:"user_id"`
 	MMPercent		float32 `json:"mm_percent"`
 	BondPercent		float32 `json:"bond_percent"`
 	StockPercent	float32 `json:"stock_percent"`
